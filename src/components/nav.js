5ccf165b0d91bbcadd388b78e116b8c726f64431
Kishan Gajera <me@kgajera.com>
2021-11-06 22:53:57 -0400

    feat: pull navigation from contentful

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,15 +1,27 @@
 import * as React from 'react';
-import { Link } from 'gatsby';
+import { Link, graphql, useStaticQuery } from 'gatsby';
 import Container from './container';
 
-const links = [
-  {
-    label: 'Blog',
-    link: '/blog',
-  },
-];
-
 const Navigation = () => {
+  const { contentfulNavigation } = useStaticQuery(graphql`
+    query {
+      contentfulNavigation(slug: {eq: "header-navigation"}) {
+        navigationItems {
+          ... on ContentfulNavigationItem {
+            id
+            slug
+            title
+          }
+          ... on ContentfulPage {
+            id
+            title
+            slug
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <Container
       as="nav"
@@ -19,15 +31,19 @@ const Navigation = () => {
     >
       <div className="flex-grow">
         <Link className="flex items-center" to="/">
-          <img className="max-h-8 inline-block mr-2" src="/icon.png" alt="Logo" />
+          <img
+            className="max-h-8 inline-block mr-2"
+            src="/icon.png"
+            alt="Logo"
+          />
           Gatsby Contentful Starter
         </Link>
       </div>
       <ul>
-        {links.map((link) => (
-          <li className="inline-block px-3">
-            <Link className="text-blue-600" to={link.link}>
-              {link.label}
+        {contentfulNavigation.navigationItems.map((item, i) => (
+          <li key={i} className="inline-block px-3">
+            <Link className="text-blue-600" to={item.slug}>
+              {item.title}
             </Link>
           </li>
         ))}
