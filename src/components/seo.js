0c561287eab17b311470147080c3fba25686452f
Kishan Gajera <me@kgajera.com>
2021-11-10 20:53:34 -0500

    feat: use site metadata to provide default values for SEO

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,17 +1,38 @@
 import * as React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 import { Helmet } from 'react-helmet';
 
-const Seo = ({ description = '', lang = 'en', title }) => {
+const Seo = ({ description = '', image, lang = 'en', title }) => {
+  const { site } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          description
+          title
+        }
+      }
+    }
+  `);
+
+  const defaultTitle = site.siteMetadata.title;
+  const metaDescription = description || site.siteMetadata.description;
+
   return (
     <Helmet
       htmlAttributes={{
         lang,
       }}
       title={title}
+      defaultTitle={defaultTitle}
+      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
       meta={[
         {
           name: 'description',
-          content: description,
+          content: metaDescription,
+        },
+        {
+          name: 'image',
+          content: image,
         },
         {
           property: 'og:title',
@@ -19,7 +40,7 @@ const Seo = ({ description = '', lang = 'en', title }) => {
         },
         {
           property: 'og:description',
-          content: description,
+          content: metaDescription,
         },
       ]}
     />
