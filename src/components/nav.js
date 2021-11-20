def7a5f976ad9f0fc27dcfd57122cdd2f71a386c
Kishan Gajera <me@kgajera.com>
2021-11-20 15:47:44 -0500

    feat: responsive nav

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,6 +4,7 @@ import Container from './container';
 import Link from './link';
 
 const Navigation = () => {
+  const [menuHidden, setMenuHidden] = React.useState(true);
   const { contentfulNavigation, site } = useStaticQuery(graphql`
     query {
       site {
@@ -32,11 +33,22 @@ const Navigation = () => {
   return (
     <Container
       as="nav"
-      className="flex items-center py-5 mb-5 border-b"
+      className="flex flex-col md:flex-row md:items-center py-5 mb-5 border-b"
       role="navigation"
       aria-label="Main"
     >
-      <div className="flex-grow">
+      <div className="flex flex-grow items-center">
+        <button
+          className="mr-4 md:hidden"
+          type="button"
+          onClick={() => setMenuHidden(!menuHidden)}
+        >
+          <svg className="w-6 h-6" viewBox="0 0 100 70">
+            <rect width="100%" height="10"></rect>
+            <rect y="30" width="100%" height="10"></rect>
+            <rect y="60" width="100%" height="10"></rect>
+          </svg>
+        </button>
         <Link className="flex items-center" to="/">
           <img
             className="max-h-8 inline-block mr-2"
@@ -46,11 +58,14 @@ const Navigation = () => {
           {site.siteMetadata.title}
         </Link>
       </div>
-      <ul>
+      <ul className={`${menuHidden ? 'hidden' : 'mt-6 md:mt-0'} md:block`}>
         {contentfulNavigation.navigationItems.map((item, i) => (
-          <li key={i} className="inline-block px-3">
+          <li
+            key={i}
+            className="block md:inline-block my-2 md:my-0 px-0 md:px-3"
+          >
             <Link
-              className="text-blue-600"
+              className="block text-blue-600"
               to={item.slug ? `/${item.slug}` : item.url}
             >
               {item.title}
